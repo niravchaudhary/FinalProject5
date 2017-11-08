7a5c4599f8d06111a0b8f3620ef13a397327580b
Nirav <nirav@email.com>
2017-11-08 20:46:35 +0000

    Added upcoming model and than deleted it modified routes alos

--- app/models/upcoming.rb
+++
@@ -1,2 +0,0 @@
-class Upcoming < ApplicationRecord
-end
--- test/models/upcoming_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UpcomingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
