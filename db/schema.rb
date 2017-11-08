61cd46415b93c9fc422fcf6d7eb9a5b2b3b771b5
Nirav <nirav@email.com>
2017-11-08 08:17:13 +0000

    Added  image columne to the Movie table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106075530) do
+ActiveRecord::Schema.define(version: 20171108081200) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20171106075530) do
     t.datetime "updated_at", null: false
     t.integer "customer_id"
     t.string "name"
+    t.string "image"
     t.index ["customer_id"], name: "index_movies_on_customer_id"
   end
 
