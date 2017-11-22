c1ed1f53c808d91ead96018e28c73ecbda4e9342
Nirav <nirav@email.com>
2017-11-22 20:59:02 +0000

    generated a image uploader

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :customer
   has_one :trailer
+  mount_uploader :image, ImageUploader
   validates :movie_name, :description, :order_price, :release_date, :star_cast, :name, presence: true
 end
