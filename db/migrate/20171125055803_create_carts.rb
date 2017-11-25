47326d7d196731db1c9eae440f050b4e62584a59
Nirav <nirav@email.com>
2017-11-25 06:03:51 +0000

    deleted the cart model because it was created incorrectly

--- db/migrate/20171125055803_create_carts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCarts < ActiveRecord::Migration[5.1]
-  def change
-    create_table :carts do |t|
-      t.string :cart
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120031251) do
+ActiveRecord::Schema.define(version: 20171125055803) do
 
   create_table "abouts", force: :cascade do |t|
     t.text "info"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 20171120031251) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.string "cart"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "contacts", force: :cascade do |t|
